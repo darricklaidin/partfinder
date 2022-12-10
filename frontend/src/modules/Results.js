95f9b55f091bb2328b59156441f784fd83e4627e
darricklaidin <darricklaidin@gmail.com>
2022-12-10 20:02:12 +0700

    Remove duplicate part numbers

--- frontend/src/modules/Results.js
+++ frontend/src/modules/Results.js
@@ -5,12 +5,19 @@ export default class Results extends Component {
   render() {
     const { filteredList } = this.props;
     
+    // Get the part numbers from the filtered list
+    const partNumbers = filteredList.map((part) => {
+      return part["PartNo"];
+    });
+    // Remove duplicate part numbers
+    const uniquePartNumbers = Array.from(new Set(partNumbers));
+    
     return (
       <div className="results">
         
-        { filteredList.length === 0 ? <div> No results found </div> : filteredList.map((part, index) => {
+        { filteredList.length === 0 ? <div> No results found </div> : uniquePartNumbers.map((partNo) => {
           return (
-            <div className="result" key={uniqid()}>{part["PartNo"]}</div>
+            <div className="result" key={uniqid()}>{partNo}</div>
           );
         }) }
         
