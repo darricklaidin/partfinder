1578214d17be82a2f27478ee68af441e73111030
darricklaidin <darricklaidin@gmail.com>
2022-12-10 18:46:44 +0700

    Implement dynamic generation in results box

--- frontend/src/modules/PartFinder.js
+++ frontend/src/modules/PartFinder.js
@@ -3,11 +3,37 @@ import FilterBox from "./FilterBox.js";
 import Results from "./Results.js";
 
 export default class PartFinder extends Component {
+  
+  constructor() {
+    super();
+    this.state = {
+      make: "",
+      model: "",
+      type: "",
+      filteredList: []
+    };
+  }
+  
+  componentDidMount() {
+    this.updateFilteredList();
+  }
+  
+  // Update the filtered list of parts state
+  async updateFilteredList() {
+    const response = await fetch(`http://partfinder.test/api/parts?make=${this.state.make}&model=${this.state.model}&type=${this.state.type}`);
+    if (response.status !== 200) {
+      console.log("Error");
+      return;
+    }
+    const data = await response.json();
+    this.setState({ filteredList: data });
+  }
+  
   render() {
     return (
       <div className="partFinder">
-        <FilterBox />
-        <Results />
+        <FilterBox make={this.state.make} model={this.state.model} type={this.state.type} />
+        <Results filteredList={this.state.filteredList}/>
       </div>
     );
   }
--- frontend/src/modules/Results.js
+++ frontend/src/modules/Results.js
@@ -1,21 +1,18 @@
 import React, { Component } from "react";
+import uniqid from "uniqid";
 
 export default class Results extends Component {
   render() {
+    const { filteredList } = this.props;
+    
     return (
       <div className="results">
         
-        {/* These will be dynamically generated */}
-        <div className="result">AA6327237X</div>
-        <div className="result">AA6327237X</div>
-        <div className="result">AA6327237X</div>
-        <div className="result">AA6327237X</div>
-        <div className="result">AA6327237X</div>
-        <div className="result">AA6327237X</div>
-        <div className="result">AA6327237X</div>
-        <div className="result">AA6327237X</div>
-        <div className="result">AA6327237X</div>
-        <div className="result">AA6327237X</div>
+        { filteredList.length === 0 ? <div> No results found </div> : filteredList.map((part, index) => {
+          return (
+            <div className="result" key={uniqid()}>{part["PartNo"]}</div>
+          );
+        }) }
         
       </div>
     );
